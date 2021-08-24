523d8668661d1f82e32c78b8993fc4196af3c032
iluxandr0 <inik3004@gmail.com>
2021-08-24 10:21:28 +0500

    add moreIcon to BookActionsList

--- src/components/Book/BookActionItem.tsx
+++ src/components/Book/BookActionItem.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { useActions } from "../../hooks/useActions";
-import { currentOpeningTabTypes } from "../../types/app";
 
 interface OpenItemProps {
   icon: any;
   name: string;
-  showPanel: currentOpeningTabTypes;
+  onClick: () => void;
 }
 
-const BookActionItem: React.FC<OpenItemProps> = ({ icon, name, showPanel }) => {
-  const { setCurrentOpeningTabRedux } = useActions();
-
-  function clickToChangeCurrentTab() {
-    setCurrentOpeningTabRedux(showPanel);
-  }
-
+const BookActionItem: React.FC<OpenItemProps> = ({ icon, name, onClick }) => {
   return (
-    <div className="open__icon" onClick={clickToChangeCurrentTab}>
+    <div className="open__icon" onClick={onClick}>
       <div className="open__icon-image">{icon}</div>
       <div className="open__icon-text">{name}</div>
     </div>
